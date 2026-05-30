c4ce45e488a739a83ec0afdbaa5267d0b300fb35
Stéphane Kergall <stef.kergall@gmail.com>
2026-05-30 22:06:37 +0200

    Sauvegarde

--- data_gesture.py
+++ data_gesture.py
@@ -10,42 +10,43 @@ app.title("Gestion des données de Lilypond")
 app.geometry("550x600")
 
 PWD = Path(__file__).resolve().parent
-repo = git.Repo(PWD)
 
+#----------------------------------
+# Sauvegarde Git
 
-def ctk_git_label(text):
-	ctk.CTkLabel(liste_fichiers, text=text).pack(fill="x")
+REPO = git.Repo(PWD)
 
+def ctk_git_label(text, bold=False):
+	ctk.CTkLabel(liste_fichiers, text=text, font=ctk.CTkFont(family="Arial", size=16, weight="bold" if bold else "normal")).pack(fill="x")
 
 def ctk_git_status():
-	ctk_git_label("Fichiers modifiés")
+	ctk_git_label("Fichiers modifiés", True)
 
-	for file in repo.index.diff(None):
+	for file in REPO.index.diff(None):
 		ctk_git_label(f"{file.a_path}")
 	
-	ctk_git_label("\nNouveaux fichiers")
+	ctk_git_label("\nNouveaux fichiers", True)
 
-	for file in repo.untracked_files:
+	for file in REPO.untracked_files:
 		ctk_git_label(f"{file}")
 
 
 def ly_save():
-	
 	ctk_git_status()
-	repo.index.commit("Modifications")
-	repo.git.add(".")
-	repo.index.commit("Sauvegarde")
+	REPO.index.commit("Modifications")
+	REPO.git.add(".")
+	REPO.index.commit("Sauvegarde")
 	
 	try:
-		repo.remote(name="origin").pull()
-		repo.remote(name="origin").push()
+		REPO.remote(name="origin").pull()
+		REPO.remote(name="origin").push()
 	except git.exc.GitCommandError:
 		git_config = git.Git().config
-		ctk_git_label("Configuration de la sauvegarde nécessaire...")
+		ctk_git_label("\nConfiguration de la sauvegarde nécessaire...")
 		git_config("user.name", "Stéphane Kergall")
 		git_config("user.email", "stef.kergall@gmail.com")
-		repo.remote(name="origin").pull()
-		repo.remote(name="origin").push()
+		REPO.remote(name="origin").pull()
+		REPO.remote(name="origin").push()
 	finally:
 		ctk_git_label("Sauvegarde terminée.")
 
@@ -54,6 +55,8 @@ btn_save.pack(pady=10)
 
 liste_fichiers = ctk.CTkScrollableFrame(app)
 liste_fichiers.pack(pady=10, fill="both", expand=True)
+
+#----------------------------------
 
 
 app.mainloop()
