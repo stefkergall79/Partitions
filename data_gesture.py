7c8385261027fb9f019b52790ae32760ea0f75c0
Stéphane Kergall <stef.kergall@gmail.com>
2026-05-30 21:19:28 +0200

    Sauvegarde

---
+++ data_gesture.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+from pathlib import Path
+import tkinter as tkt
+import git
+
+app = tkt.Tk()
+app.title("Gestion des données de Lilypond")
+app.geometry("550x600")
+
+PWD = Path(__file__).resolve().parent
+
+def ly_save():
+	pwd=git.Repo(PWD)
+	
+	pwd.index.commit("Modifications")
+	pwd.git.add(".")
+	pwd.index.commit("Sauvegarde")
+	
+	try:
+		pwd.remote(name="origin").push()
+	except git.exc.GitCommandError:
+		git_config = git.Git().config
+		git_config("--global", "user.name", "Stéphane Kergall")
+		git_config("--global", "user.email", "stef.kergall@gmail.com")
+		pwd.remote(name="origin").push()
+
+btn_save = tkt.Button(app, text="Sauvegarde", command=ly_save)
+btn_save.pack()
+
+app.mainloop()
