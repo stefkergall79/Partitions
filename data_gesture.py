b41d3fa94c6d76694cd475673b5e8adb6e5de771
Stéphane Kergall <stef.kergall@gmail.com>
2026-05-30 21:55:49 +0200

    Sauvegarde

--- data_gesture.py
+++ data_gesture.py
@@ -10,24 +10,42 @@ app.title("Gestion des données de Lilypond")
 app.geometry("550x600")
 
 PWD = Path(__file__).resolve().parent
+repo = git.Repo(PWD)
+
+def ctk_git_status():
+	ctk.CTkLabel(liste_fichiers, text="Fichiers modifiés").pack()
+
+	for file in repo.index.diff(None):
+		ctk.CTkLabel(liste_fichiers, text=f"{file.a_path}\n").pack()
+	
+	ctk.CTkLabel(liste_fichiers, text="Nouveaux fichiers").pack()
+
+	for file in repo.untracked_files:
+		ctk.CTkLabel(liste_fichiers, text=f"{file}\n").pack()
+
 
 def ly_save():
-	pwd=git.Repo(PWD)
 	
-	pwd.git.add(".")
-	pwd.index.commit("Modifications")
+	ctk_git_status()
+	repo.index.commit("Modifications")
+	repo.git.add(".")
+	repo.index.commit("Sauvegarde")
 	
 	try:
-		pwd.remote(name="origin").pull()
-		pwd.remote(name="origin").push()
+		repo.remote(name="origin").pull()
+		repo.remote(name="origin").push()
 	except git.exc.GitCommandError:
 		git_config = git.Git().config
 		git_config("--global", "user.name", "Stéphane Kergall")
 		git_config("--global", "user.email", "stef.kergall@gmail.com")
-		pwd.remote(name="origin").pull()
-		pwd.remote(name="origin").push()
+		repo.remote(name="origin").pull()
+		repo.remote(name="origin").push()
 
 btn_save = ctk.CTkButton(app, text="Sauvegarde", command=ly_save)
-btn_save.pack()
+btn_save.pack(pady=10)
+
+liste_fichiers = ctk.CTkScrollableFrame(app)
+liste_fichiers.pack(pady=10, fill="both", expand=True)
+
 
 app.mainloop()
