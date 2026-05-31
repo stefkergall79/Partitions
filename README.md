c584f4622fd14f0da85627453345595074d4191c
Stéphane Kergall <stef.kergall@gmail.com>
2026-05-31 15:34:23 +0200

    Sauvegarde

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Installation
+
+
+# Sauvegarde
 Pour sauvegarder votre travail, lancez `python3 ~/Partitions/data_gesture.py`.
---
+++ install.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("sudo apt update && sudo apt install pip python3-tk")
+os.system("pip install customtkinter gitpython --break-system-packages")
