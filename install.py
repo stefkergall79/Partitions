bd1bdc35d914b6406ec83f2c60358f9c462cb1ae
Stéphane Kergall <stef.kergall@gmail.com>
2026-05-31 21:39:49 +0200

    Sauvegarde

--- install.py
+++ install.py
@@ -1,3 +1,7 @@
 from os import system
 system("sudo apt update && sudo apt install pip python3-tk")
 system("pip install customtkinter gitpython --break-system-packages")
+import git
+git_config = git.Git().config
+git_config("user.name", "Stéphane Kergall")
+git_config("user.email", "stef.kergall@gmail.com")
