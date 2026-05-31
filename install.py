559a63dec871d4b3821733c160d2f04fbadabb23
Stéphane Kergall <stef.kergall@gmail.com>
2026-05-31 15:36:57 +0200

    Sauvegarde

--- install.py
+++ install.py
@@ -1,4 +1,3 @@
-import os
-
-os.system("sudo apt update && sudo apt install pip python3-tk")
-os.system("pip install customtkinter gitpython --break-system-packages")
+from os import system
+system("sudo apt update && sudo apt install pip python3-tk")
+system("pip install customtkinter gitpython --break-system-packages")
